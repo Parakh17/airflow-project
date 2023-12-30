d9badc4a6a9add77aae73b89a27fdd0dcf047d8c
Parakh Singhal <parakhsinghal2000@gmail.com>
2023-12-30 22:53:00 +0530

    kafka stream

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -5,3 +5,23 @@ default_args = {
     'owner': 'airscholar',
     'start_date': datetime(2023, 12, 30, 10,00)
 }
+
+
+def stream_data():
+    import json
+    import requests
+    res = requests.get("https://randomuser.me/api/")
+    res = res.json()
+
+    res = res['results'][0]
+    return res
+
+
+def format_data(res):
+    data = {}
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = res['street']['number'] + 
+
+stream_data()
