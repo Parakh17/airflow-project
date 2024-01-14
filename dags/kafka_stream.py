31cd5f70aa850978bed46b54dbf8d75247c89ee6
Parakh Singhal <parakhsinghal2000@gmail.com>
2024-01-14 21:47:00 +0530

    update kafka stream

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,5 +1,9 @@
 from datetime import datetime
 from airflow import DAG
+import uuid
+import json
+import requests
+from kafka 
 
 default_args = {
     'owner': 'airscholar',
@@ -7,9 +11,8 @@ default_args = {
 }
 
 
-def stream_data():
-    import json
-    import requests
+def get_data():
+    
     res = requests.get("https://randomuser.me/api/")
     res = res.json()
 
@@ -17,11 +20,27 @@ def stream_data():
     return res
 
 
+
 def format_data(res):
+    
     data = {}
+    data['id'] = uuid.uuid4()
+    location = res['location']
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
-    data['address'] = res['street']['number']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                        f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code']= location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+def stream_data()
 
 stream_data()
