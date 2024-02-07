ec4bbebe10e3342e0c2b0052a22e50d8e0e991fb
Parakh Singhal <parakhsinghal2000@gmail.com>
2024-02-07 15:09:21 +0530

    update

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -3,7 +3,9 @@ from airflow import DAG
 import uuid
 import json
 import requests
-from kafka 
+#from kafka import KafkaProducer
+from kafka import KafkaProducer
+from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'airscholar',
@@ -24,14 +26,14 @@ def get_data():
 def format_data(res):
     
     data = {}
-    data['id'] = uuid.uuid4()
     location = res['location']
+    data['id'] = uuid.uuid4()
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
     data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
-                        f"{location['city']}, {location['state']}, {location['country']}"
-    data['post_code']= location['postcode']
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
     data['email'] = res['email']
     data['username'] = res['login']['username']
     data['dob'] = res['dob']['date']
@@ -41,6 +43,26 @@ def format_data(res):
 
     return data
 
-def stream_data()
+def stream_data():
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res))
+    
+
+    # res = requests.get("https://randomuser.me/api/")
+    # res= res.json()
+    # res=res['results'][0]
+    # print(json.dumps(res, indent=3))
+
+
+
+# with DAG("user_automation", 
+#          default_args=default_args,
+#          schedule_interval = '@daily',
+#          catchup=False) as dag:
+#     streaming_task = PythonOperator(
+#         task_id = 'stream_data_from_api',
+#         python_callable=stream_data
+#     )
 
 stream_data()
